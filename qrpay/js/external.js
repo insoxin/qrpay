f6894983eeac061c7f6a505b7aed94ea2c9fa325
姬长信 <192666378@qq.com>
2019-04-03 12:02:43 +0800

    Update external.js

--- qrpay/js/external.js
+++ qrpay/js/external.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?fda02e31e77cd366043a929e3043c19f";
+    hm.src = "//hm.baidu.com/hm.js?b5b8aa2a84691a03c43b6bd5234ad4f9";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
